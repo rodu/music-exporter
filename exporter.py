9c7c98337e8223beba610add2504945a5ac08711
rodu <greenheron35@gmail.com>
2014-07-14 17:09:04 +0100

    Refactoring

--- exporter.py
+++ exporter.py
@@ -2,74 +2,39 @@ from xml.sax import make_parser, handler
 
 class ParserContentHandler(handler.ContentHandler):
 
+    def _get_parsed_names(self):
+        return {
+            "Genre": "",
+            "Track ID": 0,
+            "Artist": "",
+            "Name": ""
+        }
+
     def __init__(self):
         self._data_dictionary = []
-        self._tags = {
-            "dict": 0,
-            "key": 0,
-            "string": 0,
-            "integer": 0
-        }
-        self._is_key = 0
-        self._is_value = 0
-        
-        self._is_dict = 0
+        self._tags = ["dict", "key", "string", "integer"]
+        self._parsed_flags = self._get_parsed_names().keys()
+        self._parsed_values = self._get_parsed_names()
         self._is_content_dict = 0
-        self._is_genre = 0
-        self._is_track_id = 0
-        self._is_artist = 0
-        self._is_name = 0
-        
         self._entries = {
             "Unknown": []
         }
-        self._track_id = 0
-        self._genre = ""
-        self._artist = ""
-        self._name = ""
+        self._current = ""
 
-    def startElement(self, name, attrs):
-        if (name in self._tags):
-            self._tags[name] = 1
+    #def startElement(self, name, attrs):
 
     def characters(self, content):
-        if (content is None):
+        if (not content):
             return
         
-        if (content == "Genre"):
-            self._is_genre = self._is_content_dict = 1
+        if (content in self._parsed_flags):
+            self._is_content_dict = 1
+            self._current = content
             return
 
-        if (content == "Track ID"):
-            self._is_track_id = self._is_content_dict = 1
-            return
-
-        if (content == "Artist"):
-            self._is_artist = self._is_content_dict = 1
-            return
-
-        if (content == "Name"):
-            self._is_name = self._is_content_dict = 1
-            return
-
-        if (self._is_genre and self._tags["string"]):
-            # Adds the genre as key of a new set of traks if it does not exists
-            self._genre = self.optimiseContent(content)
-            if (self._genre not in self._entries):
-                # Adds a new genre to the dictionary
-                self._entries[self._genre] = []
-            return
-
-        if (self._is_track_id and self._tags["integer"]):
-            self._track_id = content
-            return
-
-        if (self._is_artist and self._tags["string"]):
-            self._artist = self.optimiseContent(content)
-            return
-
-        if (self._is_name and self._tags["string"]):
-            self._name = self.optimiseContent(content)
+        if (self._current and (self._current in self._parsed_flags)):
+            self._parsed_values[self._current] = self.optimiseContent(content)
+            self._current = ""
             return
 
     def optimiseContent(self, content):
@@ -78,39 +43,26 @@ class ParserContentHandler(handler.ContentHandler):
         return "Unknown"
 
     def endElement(self, name):
-        #if (name in self._tags):
-        #    self._tags[name] = 0
         if (name in self._tags):
-            if (self._is_genre and name == "string"):
-                self._is_genre = 0
-                return
-
-            if (self._is_track_id and name == "integer"):
-                self._is_track_id = 0
-                return
-
-            if (self._is_artist and name == "string"):
-                self._is_artist = 0
-                return
-
-            if (self._is_name and name == "string"):
-                self._is_name = 0
+            #self._tags[name] = 0
+            if (name in self._parsed_flags):
                 return
 
             if (name == "dict"):
-                self._is_dict = 0
                 if (self._is_content_dict):
                     # save the created entry
-                    if (self._genre or self._artist or self._name):
-                        self._entries[(self._genre or "Unknown")].append({
-                            "track_id": int(self._track_id),
-                            "artist": self._artist,
-                            "name": self._name
-                        })
-                        self._track_id = 0
-                        self._artist = ""
-                        self._name = ""
-                        self._is_content_dict = 0
+                    genre = (self._parsed_values["Genre"] or "Unknown")
+                    if (genre not in self._entries):
+                        self._entries[genre] = []
+
+                    self._entries[genre].append({
+                        "track_id": self._parsed_values["Track ID"],
+                        "artist": self._parsed_values["Artist"],
+                        "name": self._parsed_values["Name"]
+                    })
+                    self._parsed_values = self._get_parsed_names()
+                    self._is_content_dict = 0
+                    self._current = ""
 
 
     def store(self, list, key, value):
@@ -118,6 +70,7 @@ class ParserContentHandler(handler.ContentHandler):
 
     def get_data_dictionary(self):
         self._data_dictionary.append(self._entries)
+        #print self._data_dictionary
         return self._data_dictionary
 
 class Exporter():
