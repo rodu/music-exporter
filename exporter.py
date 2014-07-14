e130b4dd67113830b0e6a96a8dab60e7a8a7dce5
rodu <greenheron35@gmail.com>
2014-07-14 15:49:31 +0100

    Parses genre, artist and track name

--- exporter.py
+++ exporter.py
@@ -4,46 +4,120 @@ class ParserContentHandler(handler.ContentHandler):
 
     def __init__(self):
         self._data_dictionary = []
-        self._genre_count = 0
         self._tags = {
+            "dict": 0,
             "key": 0,
-            "string": 0
+            "string": 0,
+            "integer": 0
         }
         self._is_key = 0
         self._is_value = 0
+        
+        self._is_dict = 0
+        self._is_content_dict = 0
         self._is_genre = 0
-        self._genres = {}
+        self._is_track_id = 0
+        self._is_artist = 0
+        self._is_name = 0
+        
+        self._entries = {
+            "Unknown": []
+        }
+        self._track_id = 0
+        self._genre = ""
+        self._artist = ""
+        self._name = ""
 
     def startElement(self, name, attrs):
         if (name in self._tags):
             self._tags[name] = 1
 
     def characters(self, content):
-        if (content is not None and content.lower() == "genre"):
-            self._genre_count += 1
-            self._is_genre = 1
+        if (content is None):
+            return
+        
+        if (content == "Genre"):
+            self._is_genre = self._is_content_dict = 1
+            return
+
+        if (content == "Track ID"):
+            self._is_track_id = self._is_content_dict = 1
+            return
+
+        if (content == "Artist"):
+            self._is_artist = self._is_content_dict = 1
+            return
+
+        if (content == "Name"):
+            self._is_name = self._is_content_dict = 1
             return
 
         if (self._is_genre and self._tags["string"]):
             # Adds the genre as key of a new set of traks if it does not exists
-            if (content not in self._genres):
+            self._genre = self.optimiseContent(content)
+            if (self._genre not in self._entries):
                 # Adds a new genre to the dictionary
-                self._genres[self.optimiseContent(content)] = []
+                self._entries[self._genre] = []
+            return
+
+        if (self._is_track_id and self._tags["integer"]):
+            self._track_id = content
+            return
+
+        if (self._is_artist and self._tags["string"]):
+            self._artist = self.optimiseContent(content)
+            return
+
+        if (self._is_name and self._tags["string"]):
+            self._name = self.optimiseContent(content)
+            return
 
     def optimiseContent(self, content):
-        return content.replace("/", "-")
+        if (content):
+            return content.replace("/", "-")
+        return "Unknown"
 
     def endElement(self, name):
+        #if (name in self._tags):
+        #    self._tags[name] = 0
         if (name in self._tags):
-            self._tags[name] = 0
-        if (self._is_genre and name == "string"):
-            self._is_genre = 0
+            if (self._is_genre and name == "string"):
+                self._is_genre = 0
+                return
+
+            if (self._is_track_id and name == "integer"):
+                self._is_track_id = 0
+                return
+
+            if (self._is_artist and name == "string"):
+                self._is_artist = 0
+                return
+
+            if (self._is_name and name == "string"):
+                self._is_name = 0
+                return
+
+            if (name == "dict"):
+                self._is_dict = 0
+                if (self._is_content_dict):
+                    # save the created entry
+                    if (self._genre or self._artist or self._name):
+                        self._entries[(self._genre or "Unknown")].append({
+                            "track_id": int(self._track_id),
+                            "artist": self._artist,
+                            "name": self._name
+                        })
+                        self._track_id = 0
+                        self._artist = ""
+                        self._name = ""
+                        self._is_content_dict = 0
+
 
     def store(self, list, key, value):
         pass
 
     def get_data_dictionary(self):
-        self._data_dictionary.append(self._genres)
+        self._data_dictionary.append(self._entries)
         return self._data_dictionary
 
 class Exporter():
--- exporterTest.py
+++ exporterTest.py
@@ -23,11 +23,46 @@ class ExporterTest(unittest.TestCase):
       dictionary is built as expected given a testing data set
     """
     expected = [{
+      "Unknown": [],
       "Blues": []
     }]
     actual = self.exporter._build_data_dictionary("test_data.xml")
-    for i in range(len(actual)):
-      self.assertEquals(actual[i].keys(), expected[i].keys())
+    
+    self.assertTrue("Blues" in actual[0])
+    self.assertEquals(len(actual), len(expected),
+      "The list should have the correct number of items")
+    self.assertEquals(actual[0].keys(), expected[0].keys(),
+      "The second item should have the expected key")
+
+  def test_parsing_artist(self):
+    """
+      The test ensures that the artist name is parsed correctly and returned
+      in the final dictionary as expected.
+    """
+    expected = [{
+      "Unknown": [],
+      "Blues": [{
+          "track_id": 3566,
+          "artist": "Hysteric Ego",
+          "name": "Want love"
+        },{
+          "track_id": 3568,
+          "artist": "Junior Jack",
+          "name": "E Samba (Rasmus Fabers Canao D"
+        }],
+    }]
+    actual = self.exporter._build_data_dictionary("test_data.xml")
+    
+    self.assertTrue("Blues" in actual[0])
+    self.assertEquals(len(expected), len(actual))
+    self.assertEquals(len(actual[0]["Blues"]), len(expected[0]["Blues"]))
+    # Checks the artist value
+    self.assertEquals(actual[0]["Blues"][0]["artist"],
+      expected[0]["Blues"][0]["artist"])
+    # Checks the name
+    self.assertEquals(actual[0]["Blues"][0]["name"],
+      expected[0]["Blues"][0]["name"])
+    #print(actual)
 
 
 if __name__ == '__main__':
